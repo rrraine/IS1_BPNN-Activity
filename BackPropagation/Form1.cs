5b2032c42286767f374868d58ece16fe4d3bddb4
rrraine <141202258+rrraine@users.noreply.github.com>
2025-11-19 00:18:14 +0800

    added "nn.run()"

--- BackPropagation/Form1.cs
+++ BackPropagation/Form1.cs
@@ -57,7 +57,86 @@ namespace BackPropagation
             */
 
             // Training for AND function
-            for (int x = 0; x < 2000; x++)
+           
+
+
+            // 16-combination of 4-inputs, separated it from the outputs
+            double[,] inputs = new double[,]
+            {
+                 {0,0,0,0}, {0,0,0,1}, {0,0,1,0}, {0,0,1,1},
+                 {0,1,0,0}, {0,1,0,1}, {0,1,1,0}, {0,1,1,1},
+                 {1,0,0,0}, {1,0,0,1}, {1,0,1,0}, {1,0,1,1},
+                 {1,1,0,0}, {1,1,0,1}, {1,1,1,0}, {1,1,1,1}
+            };
+
+            // Corresponding outputs for AND function
+            double[] outputs = new double[]
+            {
+                 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1  // Only last case (1,1,1,1) = 1
+            };
+
+
+            // Train the model as we loop through the 16 combinations
+            /*for (int epoch = 0; epoch < 2000; epoch++)
+              {
+                  for (int i = 0; i < 16; i++)
+                  {
+                      nn.setInputs(0, inputs[i, 0]);
+                      nn.setInputs(1, inputs[i, 1]);
+                      nn.setInputs(2, inputs[i, 2]);
+                      nn.setInputs(3, inputs[i, 3]);
+
+                      nn.setDesiredOutput(0, outputs[i]);
+                      nn.learn();
+                  }
+              }*/
+
+            for (int epoch = 0; epoch < 5000; epoch++)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    // Set inputs
+                    for (int j = 0; j < 4; j++)
+                    {
+                        nn.setInputs(j, inputs[i, j]);
+                    }
+
+                    // Set desired output
+                    nn.setDesiredOutput(0, outputs[i]);
+
+                    // Train
+                    nn.learn();
+                }
+            }
+
+        }
+
+        private void testBtnClick(object sender, EventArgs e)
+        {
+
+            // Testing for OR function
+            /*
+            nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+            nn.setInputs(1, Convert.ToDouble(textBox2.Text));
+            nn.run();
+            */
+
+            // Testing for AND function
+            nn.setInputs(0, Convert.ToDouble(input1.Text));
+            nn.setInputs(1, Convert.ToDouble(input2.Text));
+            nn.setInputs(2, Convert.ToDouble(input3.Text));
+            nn.setInputs(3, Convert.ToDouble(input4.Text));
+            nn.run();
+
+
+            outputN.Text = "" + nn.getOuputData(0); // convert to string
+        }
+
+     
+    }
+}
+
+/* for (int x = 0; x < 2000; x++)
             {
                 nn.setInputs(0, 0.0);
                 nn.setInputs(1, 0.0);
@@ -170,79 +249,4 @@ namespace BackPropagation
                 nn.setInputs(3, 1.0);
                 nn.setDesiredOutput(0, 1.0);
                 nn.learn();
-            }
-
-
-            // 16-combination of 4-inputs, separated it from the outputs
-            /* double[,] inputs = new double[,]
-             {
-                 {0,0,0,0}, {0,0,0,1}, {0,0,1,0}, {0,0,1,1},
-                 {0,1,0,0}, {0,1,0,1}, {0,1,1,0}, {0,1,1,1},
-                 {1,0,0,0}, {1,0,0,1}, {1,0,1,0}, {1,0,1,1},
-                 {1,1,0,0}, {1,1,0,1}, {1,1,1,0}, {1,1,1,1}
-             };
-
-             // Corresponding outputs for AND function
-             double[] outputs = new double[]
-             {
-                 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1  // Only last case (1,1,1,1) = 1
-             };
-
-
-             // Train the model as we loop through the 16 combinations
-             //for (int epoch = 0; epoch < 2000; epoch++)
-             //{
-             //    for (int i = 0; i < 16; i++)
-             //    {
-             //        nn.setInputs(0, inputs[i, 0]);
-             //        nn.setInputs(1, inputs[i, 1]);
-             //        nn.setInputs(2, inputs[i, 2]);
-             //        nn.setInputs(3, inputs[i, 3]);
-
-             //        nn.setDesiredOutput(0, outputs[i]);
-             //        nn.learn();
-             //    }
-             //}
-             for (int epoch = 0; epoch < 5000; epoch++)
-             {
-                 for (int i = 0; i < 16; i++)
-                 {
-                     // Set inputs
-                     for (int j = 0; j < 4; j++)
-                     {
-                         nn.setInputs(j, inputs[i, j]);
-                     }
-
-                     // Set desired output
-                     nn.setDesiredOutput(0, outputs[i]);
-
-                     // Train
-                     nn.learn();
-                 }
-             }*/
-
-        }
-
-        private void testBtnClick(object sender, EventArgs e)
-        {
-
-            // Testing for OR function
-            /*
-            nn.setInputs(0, Convert.ToDouble(textBox1.Text));
-            nn.setInputs(1, Convert.ToDouble(textBox2.Text));
-            nn.run();
-            */
-
-            // Testing for AND function
-            nn.setInputs(0, Convert.ToDouble(input1.Text));
-            nn.setInputs(1, Convert.ToDouble(input2.Text));
-            nn.setInputs(2, Convert.ToDouble(input3.Text));
-            nn.setInputs(3, Convert.ToDouble(input4.Text));
-
-
-            outputN.Text = "" + nn.getOuputData(0); // convert to string
-        }
-
-     
-    }
-}
+            }*/
