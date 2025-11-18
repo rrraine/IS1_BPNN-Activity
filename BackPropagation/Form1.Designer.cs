22becdd8bac2b2ec0773088ea4f1cb12e83b6bf0
rrraine <141202258+rrraine@users.noreply.github.com>
2025-11-18 23:25:21 +0800

    try

---
+++ BackPropagation/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace BackPropagation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.outputN = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.input3 = new System.Windows.Forms.TextBox();
+            this.input4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // input1
+            // 
+            this.input1.Location = new System.Drawing.Point(171, 195);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(324, 22);
+            this.input1.TabIndex = 0;
+            // 
+            // input2
+            // 
+            this.input2.Location = new System.Drawing.Point(171, 230);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(324, 22);
+            this.input2.TabIndex = 1;
+            // 
+            // outputN
+            // 
+            this.outputN.Location = new System.Drawing.Point(573, 230);
+            this.outputN.Name = "outputN";
+            this.outputN.Size = new System.Drawing.Size(324, 22);
+            this.outputN.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(171, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "create BPNN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.createNeuralNetBtnClick);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(415, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 47);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Train the Neural Net";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.trainNeuralNetBtnClick);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(659, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(238, 47);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.testBtnClick);
+            // 
+            // input3
+            // 
+            this.input3.Location = new System.Drawing.Point(171, 267);
+            this.input3.Name = "input3";
+            this.input3.Size = new System.Drawing.Size(324, 22);
+            this.input3.TabIndex = 6;
+            // 
+            // input4
+            // 
+            this.input4.Location = new System.Drawing.Point(171, 308);
+            this.input4.Name = "input4";
+            this.input4.Size = new System.Drawing.Size(324, 22);
+            this.input4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 670);
+            this.Controls.Add(this.input4);
+            this.Controls.Add(this.input3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputN);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.TextBox outputN;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox input3;
+        private System.Windows.Forms.TextBox input4;
+    }
+}
+
---
+++ BackPropagation/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Backprop;
+
+
+namespace BackPropagation
+{
+    public partial class Form1 : Form
+    {
+
+        NeuralNet nn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createNeuralNetBtnClick(object sender, EventArgs e)
+        {
+            // nn = new NeuralNet(2,100,1); // for OR function
+            nn = new NeuralNet(4, 300, 1); // for AND function
+                             // ^ input, hidden, output neurons
+        }
+
+        private void trainNeuralNetBtnClick(object sender, EventArgs e)
+        {
+            // Training for OR function
+            /*
+            for (int x = 0; x < 100; x++)
+            {
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+            }
+            */
+
+            // Training for AND function
+
+            // 1. the 16-combination of 4-inputs
+            int[,] inputs = new int[,]
+            {
+                    {0,0,0,0},
+                    {0,0,0,1},
+                    {0,0,1,0},
+                    {0,0,1,1},
+                    {0,1,0,0},
+                    {0,1,0,1},
+                    {0,1,1,0},
+                    {0,1,1,1},
+                    {1,0,0,0},
+                    {1,0,0,1},
+                    {1,0,1,0},
+                    {1,0,1,1},
+                    {1,1,0,0},
+                    {1,1,0,1},
+                    {1,1,1,0},
+                    {1,1,1,1},
+            };
+
+            double[] outputs = new double[]
+            {
+                0,0,0,0,
+                0,0,0,0,
+                0,0,0,0,
+                0,0,0,
+                1   // Only the last case (1111) is 1
+            };
+
+
+            // 2. Train the model as we loop through the 16 combinations
+            for (int epoch = 0; epoch < 100; epoch++) { 
+                for (int i = 0; i < 16; i++) 
+                {
+                    nn.setInputs(0, inputs[i, 0]);
+                    nn.setInputs(1, inputs[i, 1]);
+                    nn.setInputs(2, inputs[i, 2]);
+                    nn.setInputs(3, inputs[i, 3]);
+
+                    nn.setDesiredOutput(0, outputs[i]);
+                    nn.learn();
+                }
+            }
+
+        }
+
+        private void testBtnClick(object sender, EventArgs e)
+        {
+
+            // Testing for OR function
+            /*
+            nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+            nn.setInputs(1, Convert.ToDouble(textBox2.Text));
+            nn.run();
+            */
+
+            // Testing for AND function
+            nn.setInputs(0, Convert.ToDouble(input1.Text));
+            nn.setInputs(1, Convert.ToDouble(input2.Text));
+            nn.setInputs(2, Convert.ToDouble(input3.Text));
+            nn.setInputs(3, Convert.ToDouble(input4.Text));
+
+
+            outputN.Text = "" + nn.getOuputData(0); // convert to string
+        }
+    }
+}
